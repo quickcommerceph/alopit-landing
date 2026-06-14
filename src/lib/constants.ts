89177215b332bbaaa2dc81867cbfd539bf454850
Von <voncarcha@gmail.com>
2026-06-14 13:13:50 +0800

    update alopit link

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
-export const LOGIN_URL = "https://alopit.club/login";
-export const REGISTER_URL = "https://alopit.club/register";
+export const LOGIN_URL = "https://alopitclub.biz/login";
+export const REGISTER_URL = "https://alopitclub.biz/register";
 export const ALOWIN_URL = "https://www.alowin.club";
